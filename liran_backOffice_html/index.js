7f90622652c8fba7c30cb2cc26e1df279144be68
unknown <lirannh@gmail.com>
2018-06-06 16:18:45 +0300

    policy

--- liran_backOffice_html/index.js
+++ liran_backOffice_html/index.js
@@ -8,7 +8,6 @@ http.createServer(function (req, res) {
   fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
-      res.write(data);
       return res.end("404 Not Found");
     }  
     res.writeHead(200, {'Content-Type': 'text/html'});
