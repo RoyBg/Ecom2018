2419098721f8bbe588b62f75e72879e27097d359
Gal Shnapp <gal.shnapp@gmail.com>
2018-06-10 00:30:38 +0300

    minor change

--- app.js
+++ app.js
@@ -1,82 +1,95 @@
+// -------------------- Imports! -------------------
+// -------------------- Imports! -------------------
+// -------------------- Imports! -------------------
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
 
+// -------------------- Imports! -------------------
+// -------------------- Imports! -------------------
+// -------------------- Imports! -------------------
 
-var ideas = {
-    1 : "A Logo...",
-    2 : "Tons of cat's gifs!!!!!!"
-};
 
-app.get('/', function (req, res) {
-   res.send('Hello! \nI welcome you to my humble server.\nIf you wanna see the cool stuff, try and get some ideas ;)');
-});
 
-// /static/<filename> - returns the <filename> from the “www” directory that you should create
-app.use(express.static('www'));
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/ideaup', function(req,res){
-    // /idea/<id> (POST) - update an idea (string)  by it’s id
-    console.log("post");
-    console.log(ideas);
+// -------------------- Local Variables! -------------------
+// -------------------- Local Variables! -------------------
+// -------------------- Local Variables! -------------------
+
+var orders= [];
+
+// -------------------- Local Variables! -------------------
+// -------------------- Local Variables! -------------------
+// -------------------- Local Variables! -------------------
+
+
+
+
+
+
+
+// -------------------- Server Settings! -------------------
+// -------------------- Server Settings! -------------------
+// -------------------- Server Settings! -------------------
+
+
+app.use(express.static('Ecom2018/www')); // directory to pull pages from
+app.use(bodyParser.json()); // request parser to use
+app.use(bodyParser.urlencoded({ extended: true })); // this came from stackoverflow
+
+// -------------------- Server Settings! -------------------
+// -------------------- Server Settings! -------------------
+// -------------------- Server Settings! -------------------
+
+
+
+// -------------------- Server Routing! -------------------
+// -------------------- Server Routing! -------------------
+// -------------------- Server Routing! -------------------
+
+// Tend an incomming dispute
+app.post('/fileDispute', function(req,res) {
     var data = req.body;
-    for(var key in data){
-        ideas[key] = data[key];
-    }
-    console.log(ideas);
+    // (title, date, name_user, name_product, descript, status)
+    var ord = new Order(
+     'dispute from ' + data.name,
+     data.name,
+     data.Date,
+     data.Product,
+     data.freeText, 
+     data.status);
+
+    orders.push(ord);
+    res.send('we\'re good');
+    console.log(orders);
+    console.log(data);
+    //TODO: mail the mofo
 });
 
-
-app.get('/roro', function(req,res){
-    console.log("post");
-    console.log(ideas);
-    res.send('this is my testing roror is the king ');
-
-});
-
-
-app.delete('/idead', function(req,res){
-    // /idea/<id> (DELETE) - delete an idea by it’s id (returns 0 if success, 1 otherwise)
-    console.log("delete");
-    console.log(ideas);
-    var data = req.body;
-    for(var key in data){
-        delete ideas[key];
-    }
-    console.log(ideas);   
-});
-
-
-app.put('/ideapu', function(req,res) {
-    // /idea (PUT) - add new idea ( idea is just a string) returns the idead’s id
-    console.log("put");
-    console.log(ideas);
-    var data = req.body;
-    for(var key in data){
-        ideas[key] = data[key];
-    }
-    console.log(ideas);
-});
-
-app.get('/ideas', function (req, res) {
-    // /ideas (GET) - returns all the ideas as an object whereas id(number) -> idea(string)
+// Retrive all disputes
+app.get('/getDisputes', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(ideas)); 
-    console.log("get");
+    res.send(JSON.stringify(orders)); 
 });
+
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address;
    var port = server.address().port;
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening at http://%s:%s", host, port);
 });
 
-request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body.url);
-  console.log(body.explanation);
-});
+function Order(title, date, name_user, name_product, descript, status) {
+    this.title = title;
+    date = (date.toString()).substring(0, 25);
+    this.date = date;
+    this.name_user = name_user;
+    this.name_product = name_product;
+    this.descript = descript;
+    this.status = status;
+  
+  }
